00a15e97d962e2ad5ca3965596c6b036fee57ea6
Maurice Pasternak <maurice.pasternak@utoronto.ca>
2024-11-04 23:27:35 -0500

    feat: :building_construction: Update team, logo, dependencies. Added resource content.
    Team composition for 2024 was updated to reflect member stepping down from duty. GRC Logo established and relevant header & favicon updated to reflect this. Several resource pages updated with content in anticipation of the 2024 survey. Dependencies also updated.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 import { blogTagSchema } from './schemas'
 
 export const BLOG_TAGS = blogTagSchema.options
-export const SITE_TITLE = 'Faculty of Medicine, Graduate Representation Committee'
+export const SITE_TITLE = 'Graduate Representation Committee, Faculty of Medicine, University of Toronto'
 export const SITE_DESCRIPTION = 'The official website of the Faculty of Medicine, Graduate Representation Committee'
