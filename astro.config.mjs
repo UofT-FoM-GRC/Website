4c80437bdc1e0d05ac6f1155bd9f4401bbcb2e56
Maurice Pasternak <maurice.pasternak@utoronto.ca>
2025-10-24 22:53:52 -0400

    feat: :arrow_up: Update tailwindcss to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import alpinejs from '@astrojs/alpinejs'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-
+import tailwindcss from '@tailwindcss/vite'
 import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://uoftfomgrc.ca',
-	integrations: [mdx(), sitemap(), tailwind(), icon(), alpinejs()],
+	integrations: [mdx(), sitemap(), icon(), alpinejs()],
 	output: 'static',
-	adapter: netlify()
+	adapter: netlify(),
+	vite: {
+		plugins: [tailwindcss()]
+	}
 })
